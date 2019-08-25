cc6ef8993656871680ceae5ef58dc76f3822b288
mitrofanovdv <mitrofanovdv@pochtabank.ru>
2019-08-25 17:46:10 +0300

    set swift version

--- lib/advert_id.dart
+++ lib/advert_id.dart
@@ -3,8 +3,7 @@ import 'dart:async';
 import 'package:flutter/services.dart';
 
 class AdvertId {
-  static const MethodChannel _channel =
-      const MethodChannel('advert_id');
+  static const MethodChannel _channel = const MethodChannel('advert_id');
 
   static Future<String> get id async {
     final String id = await _channel.invokeMethod('getAdvertisingId');
