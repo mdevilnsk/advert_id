2f2d0f74f4c41cf2593ddff5da10ac34e8f01b57
mitrofanovdv <mitrofanovdv@pochtabank.ru>
2019-09-12 11:29:24 +0300

    move to objective c

--- ios/Classes/AdvertIdPlugin.h
+++ ios/Classes/AdvertIdPlugin.h
@@ -1,4 +1,8 @@
 #import <Flutter/Flutter.h>
 
 @interface AdvertIdPlugin : NSObject<FlutterPlugin>
++ (instancetype)sharedInstance;
+- (BOOL)application:(UIApplication *)application
+    continueUserActivity:(NSUserActivity *)userActivity
+ restorationHandler:(void (^)(NSArray *_Nullable))restorationHandler;
 @end
